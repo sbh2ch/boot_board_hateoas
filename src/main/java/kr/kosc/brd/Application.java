11a6d1b5bd0af1e227f2f08b3458b5efe681f1a7
손병화 <sonbyeonghwa@sonbyeonghwaui-MacBook-Pro.local>
2017-07-19 15:27:00 +0900

    initialize

---
+++ src/main/java/kr/kosc/brd/Application.java
@@ -0,0 +1,15 @@
+package kr.kosc.brd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sonbyeonghwa on 2017. 7. 19..
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
